874e480179ad1fdae90ad98236f423b6e397bf4d
hamed252252 <81410537+hamed252252@users.noreply.github.com>
2024-09-09 00:16:13 +0330

    4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Intro from "@/components/ui/intro";
 import ThemeToggle from "@/components/ui/ThemeToggle";
 import Image from "next/image";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import RecentPosts from "@/components/recent-posts";
 export default function Home() {
   const content = `
   # This markdown Heading`;
@@ -12,6 +13,9 @@ export default function Home() {
       </div>
       <div className="container">
         <Intro />
+      </div>
+      <div className="container">
+        <RecentPosts />
       </div>
     </section>
   );
---
+++ src/components/recent-posts.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { getPosts } from "@/lib/posts";
+import Posts from "@/components/posts";
+
+export default async function RecentPosts() {
+  const posts = await getPosts(4);
+  return (
+    <section className="pb-24">
+      <div>
+        <h2 className="title mb-12">Recent Posts</h2>
+        <Posts posts={posts} />
+        <Link
+          href={"/posts"}
+          className="mt-8 inline-flex items-center gap-2 text-muted-foreground"
+        >
+          <span>all posts</span>
+        </Link>
+      </div>
+    </section>
+  );
+}
